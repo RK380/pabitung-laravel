6991cce9ea65c971a2b10254f32ceb120a50b1e8
unknown <RK380>
2026-02-18 11:43:33 +0700

    updt index

--- resources/views/halaman/index.blade.php
+++ resources/views/halaman/index.blade.php
@@ -175,7 +175,8 @@
             </div>
             </div>
           </div>
-    
+    </div>
+    <div class="container copyright text-center mt-4">
       <p>© 2025 <span>Muhammad Rizky</span> <strong class="px-1 sitename">Pengadilan Agama Bitung</strong>| Mahkamah Agung Republik Indonesia</p>
     </div>
   </footer>
