d1be699eec8c445f27bafcf3d8c56f11bdb1790c
unknown <RK380>
2026-06-01 21:20:32 +0700

    updt halaman controller

--- app/Models/Perkara.php
+++ app/Models/Perkara.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Enums\StatusPerkara;
+use App\Enums\StatusOperator;
 use Carbon\Carbon;
 
 class Perkara extends Model
