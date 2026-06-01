ecd252c7e16fa073eb3fd768e3af8cceb02962bb
unknown <RK380>
2026-06-01 11:07:06 +0700

    updt operator blade

--- resources/views/halaman/operator.blade.php
+++ resources/views/halaman/operator.blade.php
@@ -89,7 +89,7 @@
                                             <option value="Kewarisan" @selected(old('jenis') == 'Kewarisan')>Kewarisan</option>
                                             <option value="Harta Bersama" @selected(old('jenis') == 'Harta Bersama')>Harta Bersama</option>
                                             <option value="Permohonan Perwalian" @selected(old('jenis') == 'Permohonan Perwalian')>Permohonan Perwalian</option>
-                                            <option value="Itsbat Gugatan" @selected(old('jenis') == 'Itsbat Gugatan')>Itsbat Gugatan</option>
+                                            <option value="Isbat Gugatan" @selected(old('jenis') == 'Isbat Gugatan')>Isbat Gugatan</option>
                                     </select>
                                     <p style="font-size:12px;color:red;padding-top:5px;">* Jenis Perkara Wajib Di Isi</p>
                                 </div>
