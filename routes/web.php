12184f00820e8e983f4e9eec0c2ebcead46a859f
unknown <RK380>
2026-06-02 15:46:07 +0700

    updt routes, and 3 files more

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HalamanController::class, 'index'])->name('home');
+Route::get('/monperkara', [HalamanController::class, 'index'])->name('monperkara');
+Route::get('/monipendis', [HalamanController::class, 'index'])->name('monipendis');
+Route::get('/monpihakmed', [HalamanController::class, 'index'])->name('monpihakmed');
 Route::get('/statistik-pengunjung', [VisitorController::class, 'index'])->name('visitors.index');
 
     //LOGIN & LOGOUT
