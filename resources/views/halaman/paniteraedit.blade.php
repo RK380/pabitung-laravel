607596848ea23d660efa4aabcafabf2943487926
unknown <RK380>
2026-09-07 14:23:31 +0700

    updt paniteraedit blade

--- resources/views/halaman/paniteraedit.blade.php
+++ resources/views/halaman/paniteraedit.blade.php
@@ -69,13 +69,13 @@
 
                                             <div class="col-md-6 ">
                                                 <label for="lokasipemohon">Lokasi Pemohon/Penggugat</label>
-                                                <input type="text" class="form-control" name="lokasipemohon"
-                                                    value="{{ old('lokasi_pemohon', $data->lokasi_pemohon) }}" disabled>
+                                                <textarea class="form-control" name="lokasipemohon"
+                                                    value="{{ old('lokasi_pemohon', $data->lokasi_pemohon) }}" disabled></textarea>
                                             </div>
                                             <div class="col-md-6 ">
                                                 <label for="lokasitergugat">Lokasi Tergugat</label>
-                                                <input type="text" class="form-control" name="lokasitergugat"
-                                                    value="{{ old('lokasi_tergugat', $data->lokasi_tergugat) }}" disabled>
+                                                <textarea class="form-control" name="lokasitergugat"
+                                                    value="{{ old('lokasi_tergugat', $data->lokasi_tergugat) }}" disabled></textarea>
                                             </div>
                                             <div class="col-md-6 ">
                                                 <label for="lokasitergugat">Keterangan</label>
